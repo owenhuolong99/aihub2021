96c60c592f5ac66fe80c7e97f4f0689ec8eac72f
Owen <Owen@Owendeyuntu.local>
2021-07-24 16:11:40 +0800

    add new file: linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,9 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+x = np.linspace(0, 1)
+y = 3 * x + 4 + np.random.rand() / 100
+x = x.reshape(-1, 1)
+
+model = LinearRegression()
+model.fit(x, y)
